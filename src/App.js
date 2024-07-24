62d16568d107361b1b974560c553e0ce18885fd4
Carson Clark <carsonclark@Carsons-MacBook-Air.local>
2024-07-23 22:06:46 -0700

    add props

--- src/App.js
+++ src/App.js
@@ -48,12 +48,27 @@ const pizzaData = [
   },
 ];
 
-function Pizza() {
+// function Pizza({ name, ingredients, photoName }) {
+//   return (
+//     <div>
+//       <img src={photoName} alt={name} />
+//       <h3>{name}</h3>
+//       <p>{ingredients}</p>
+//     </div>
+//   );
+// }
+
+function Pizza(props) {
+  console.log(props);
+
   return (
-    <div>
-      <img src="pizzas/spinaci.jpg" alt="spinaci pizza" />
-      <h3>Pizza Spinaci</h3>
-      <p>Tomato, mozarella, spinach, and ricotta cheese</p>
+    <div className="pizza">
+      <img src={props.photoName} alt={props.name} />
+      <div>
+        <h3>{props.name}</h3>
+        <p>{props.ingredients}</p>
+        <span>{props.price}</span>
+      </div>
     </div>
   );
 }
@@ -62,7 +77,12 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
-      <Pizza />
+      <Pizza
+        name="Pizza Spinaci"
+        ingredients="Tomato, mozarella, spinach, and ricotta cheese"
+        photoName="pizzas/spinaci.jpg"
+        price={10}
+      />
       <Pizza />
       <Pizza />
     </main>
