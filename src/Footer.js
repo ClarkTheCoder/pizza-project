2c098bfbf586ff4b0964efe4ce56c45475e3450e
Carson Clark <carsonclark@Carsons-MacBook-Air.local>
2024-07-26 23:14:10 -0700

    extract jsx to seperate component for readability

--- src/Footer.js
+++ src/Footer.js
@@ -6,15 +6,19 @@ function Footer() {
 
   return (
     <footer className="footer">
-      {isOpen && (
-        <div className="order">
-          <p>
-            We're open until {closeHour}:00 - Come visit us, or order online.{" "}
-          </p>
-          <button className="btn">Order</button>
-        </div>
-      )}
+      <Order openHour={openHour} closeHour={closeHour} />
     </footer>
+  );
+}
+
+function Order(props) {
+  return (
+    <div className="order">
+      <p>
+        We're open until {props.closeHour}:00 - Come visit us, or order online.{" "}
+      </p>
+      <button className="btn">Order</button>
+    </div>
   );
 }
 export default Footer;
